29d3f7d6084f9d102e4bb60d87a4300be3a939b8
Sai Chaitanya <saichaitanya88@gmail.com>
2015-12-04 14:36:22 -0500

    Update sllReverse.js

--- 0010-SLLReverse/sllReverse.js
+++ 0010-SLLReverse/sllReverse.js
@@ -1,16 +1,28 @@
 //Reverse SLL without using any extra nodes
-var Node = function(params){
-	this.Name = (params && params.Name) ? params.Name : '';
-	this.Value = (params && params.Value) ? params.Value : '';
-	this.Next = (params && params.Next) ? params.Next : '';;
-}
+function reverse(){
+	Node = function(params){
+		this.Name = (params && params.Name) ? params.Name : '';
+		this.Value = (params && params.Value) ? params.Value : '';
+		this.Next = (params && params.Next) ? params.Next : null;
+	}
 
-var RootNode = new Node({Name: 'root', Value : 1 });
-var currentNode = RootNode;
+	var RootNode = new Node({Name: 'root', Value : 1 });
+	var currentNode = RootNode;
 
-// Create Nodes
-for(var i = 2; i < 20; i++){
-	var newNode = new Node({Name: 'Node: ' + i, Value : i });
-	currentNode.Next = newNode;
-	currentNode = currentNode.Next;
+	for(var i = 2; i < 20; i++){
+		var newNode = new Node({Name: 'Node: ' + i, Value : i });
+		currentNode.Next = newNode;
+		currentNode = currentNode.Next;
+	}
+
+	currentNode = RootNode;
+	var runner = currentNode.Next;
+
+	while(runner.Next != null){
+		currentNode.Next = currentNode;
+		currentNode = runner;
+		runner = runner.Next;
+		console.log(runner);
+	}
 }
+console.log("loaded");

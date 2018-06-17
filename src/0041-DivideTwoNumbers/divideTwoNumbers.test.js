8048c06b2d811b0fc393879dc6a5e54b17789d45
Sai Chaitanya <saichaitanya88@gmail.com>
2018-06-17 15:12:17 -0400

    divide two numbers

---
+++ src/0041-DivideTwoNumbers/divideTwoNumbers.test.js
@@ -0,0 +1,38 @@
+describe("Divide Two Numbers", () => {
+    it("", () => {
+        var divide = function (dividend, divisor) {
+            if (dividend == 0) {
+                return 0;
+            }
+            var isDividendNegative = false;
+            if (dividend < 0) {
+                dividend = -dividend;
+                isDividendNegative = true;
+            }
+            var isDivisorNegative = false;
+            if (divisor < 0) {
+                divisor = -divisor;
+                isDivisorNegative = true;
+            }
+            var count = 0;
+            if (dividend > 2147483647) {
+                dividend = 2147483647;
+            }
+            while (dividend >= divisor) {
+                dividend = dividend - divisor;
+                count++;
+            }
+            var isResultNegative = isDividendNegative ^ isDivisorNegative;
+            return isResultNegative ? -1 * count++ : count;
+        };
+        expect(divide(10, 3)).toBe(3);
+        expect(divide(7, -3)).toBe(-2);
+        expect(divide(-2147483648, -1)).toBe(2147483647);
+        expect(divide(-2147483648, 1)).toBe(-2147483648);  // currently this one fails
+        expect(divide(-1, -1)).toBe(1);
+        expect(divide(-1, 1)).toBe(-1);
+        expect(divide(2147483647, 1)).toBe(2147483647);
+
+        // Solves 986 / 989 cases
+    });
+})

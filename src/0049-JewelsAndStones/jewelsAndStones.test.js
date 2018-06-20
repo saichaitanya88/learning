01d897a8e25c6040db5e947cb3f335d5d98172fd
Sai Chaitanya <saichaitanya88@gmail.com>
2018-06-20 07:49:19 -0400

    added jewels and stones

---
+++ src/0049-JewelsAndStones/jewelsAndStones.test.js
@@ -0,0 +1,17 @@
+describe("Jewels and Stones", () => {
+    it("", () => {
+        var numJewelsInStones = function (J, S) {
+            var count = 0;
+            J.split("").forEach(j => {
+                S.split("").forEach(s => {
+                    if (s == j) {
+                        count++;
+                    }
+                });
+            });
+            return count;
+        };
+        expect(numJewelsInStones("aA", "aAAbbbb")).toBe(3);
+        expect(numJewelsInStones("z", "ZZ")).toBe(0);
+    });
+})

d3c8a3e790fa7f6fe1514a3e0989953760ff9378
Sai Chaitanya <saichaitanya88@gmail.com>
2018-07-08 07:16:13 -0400

    finding intersection in a linkedList with O(n) time, and O(n) space

--- src/0033-LinkedListsFindIntersectionNode/linkedListsFindIntersectionNote.test.js
+++ src/0033-LinkedListsFindIntersectionNode/linkedListsFindIntersectionNote.test.js
@@ -44,4 +44,52 @@ describe("Linked List Find Intersection Node", () => {
 
         // 42 / 42 test cases passed. but last test case is too slow. needs the O(n) solution
     });
+
+    it("Intersection of Two Linked Lists - O(n)", () => {
+        function ListNode(val) {
+            this.val = val;
+            this.next = null;
+        }
+
+        var getIntersectionNode = function (headA, headB) {
+            let aTraverser = headA;
+            while(aTraverser){
+                aTraverser.visited = true;
+                aTraverser = aTraverser.next;
+            }
+            let bTraverser = headB;
+            while(bTraverser){
+                if  (bTraverser.visited){
+                    return bTraverser;
+                }
+                bTraverser = bTraverser.next;
+            }
+            return null;
+        };
+
+        /*
+        A:          a1 → a2
+                            ↘
+                                c1 → c2 → c3
+                            ↗            
+        B:     b1 → b2 → b3
+        */
+        
+        var a = new ListNode("a1");
+        a.next = new ListNode("a2");
+        var c = new ListNode("c1");
+        a.next.next = c;
+        c.next = new ListNode("c2");
+        c.next.next = new ListNode("c3");
+        var b = new ListNode("b1")
+        b.next = new ListNode("b2");
+        b.next.next = new ListNode("b3");
+        b.next.next.next = c;
+        expect(getIntersectionNode(a, b).val).toBe("c1");
+        a = new ListNode("1")
+        b = a
+        expect(getIntersectionNode(a, b).val).toBe("1");
+
+        // 42 / 42 test cases passed. but last test case is too slow. needs the O(n) solution
+    });
 });

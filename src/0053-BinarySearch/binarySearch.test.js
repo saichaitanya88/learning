8de393a3c59d5531de7a571b4cdcdcb06b818693
Sai Chaitanya <saichaitanya88@gmail.com>
2018-07-08 05:56:56 -0400

    binary search

--- src/0053-BinarySearch/binarySearch.test.js
+++ src/0053-BinarySearch/binarySearch.test.js
@@ -1,3 +1,23 @@
 describe("BinarySearch", () => {
-    it("");
+    it("", () => {
+        var binarySearchImpl = (numbers, value, low, high) => {
+            if (high < low) {
+                return null;
+            }
+
+            var mid = Math.floor((low + high) / 2);
+
+            if (numbers[mid] > value) {
+                return binarySearchImpl(numbers, value, low, mid - 1);
+            }
+            if (numbers[mid] < value) {
+                return binarySearchImpl(numbers, value, mid + 1, high);
+            }
+            return mid;
+        }
+        var binarySearch = (numbers, value) => {
+            return binarySearchImpl(numbers, value, 0, numbers.length);
+        }
+        expect(binarySearch([1, 2, 3, 4, 5, 6, 7], 4)).toBe(3);
+    });
 });
